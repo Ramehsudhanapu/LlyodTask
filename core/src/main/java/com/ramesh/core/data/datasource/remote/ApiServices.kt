9583fb9ecdc6b99392584eea8de6267bd8d013a8
rameshs <sudhanapu.ramesh@gmai.com>
2025-01-02 10:18:40 +0530

    change name to remote

---
+++ core/src/main/java/com/ramesh/core/data/datasource/remote/ApiServices.kt
@@ -0,0 +1,20 @@
+package com.ramesh.core.data.datasource.network
+import com.ramesh.core.data.model.ProductResponse
+import com.ramesh.core.data.model.Product
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface ApiServices {
+    companion object {
+        const val BASE_URL = "https://fakestoreapi.com"
+    }
+    @GET("products/categories")
+    suspend fun getCategories(): List<String>
+
+    @GET("products/category/{categoryname}")
+    suspend fun getProductsByCategory(@Path("categoryname") categoryName: String):List<ProductResponse>
+
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
+}
