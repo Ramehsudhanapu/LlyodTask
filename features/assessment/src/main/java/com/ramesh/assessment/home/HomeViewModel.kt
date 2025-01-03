31535b89707ff6a43826d579dc3deb88079eac89
Ramesh Sudhanpu <sudhanapu.ramesh@gmail.com>
2025-01-03 17:32:36 +0530

    add strings

--- features/assessment/src/main/java/com/ramesh/assessment/home/HomeViewModel.kt
+++ features/assessment/src/main/java/com/ramesh/assessment/home/HomeViewModel.kt
@@ -1,13 +1,17 @@
 package com.ramesh.assessment.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ramesh.assessment.utility.BaseViewModel
 import com.ramesh.assessment.utility.UiState
 import com.ramesh.core.domain.usecase.GetProductsByCategoryNameUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import com.ramesh.core.data.model.ProductResponse
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -16,32 +20,38 @@ import kotlinx.coroutines.withContext
 class HomeViewModel @Inject constructor(
     private val getCategoriesUseCase: GetProductsByCategoryNameUseCase,
 
-) : ViewModel() {
+) : BaseViewModel() {
 
     val _uiStateCategory: MutableStateFlow<UiState<List<ProductResponse>>> =
         MutableStateFlow(UiState.Loading)
 
-
+    // Define a CoroutineExceptionHandler
+    private val exceptionHandler = CoroutineExceptionHandler { _, exception ->
+        _uiStateCategory.value = UiState.Error(handleException(exception))
+    }
     // get the category data from the api
     fun getAllProductsByCategory(categoryName: String) {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) { // Perform network operation in IO dispatcher
-                try {
-                    getCategoriesUseCase.execute(categoryName).onEach { category ->
-                        withContext(Dispatchers.Main) { // Switch to Main thread for UI updates
-                            _uiStateCategory.value = UiState.Success(category)
-                        }
-                    }.launchIn(this)
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) { // Ensure error updates happen on Main thread
-                        _uiStateCategory.value = UiState.Error(e.message.toString())
+        viewModelScope.launch(exceptionHandler + Dispatchers.IO) {
+            getCategoriesUseCase.execute(categoryName)
+                .flowOn(Dispatchers.IO) // Perform network operation in IO dispatcher
+                .onEach { category ->
+                    // Switch to the main thread for UI updates
+                    withContext(Dispatchers.Main) {
+                        _uiStateCategory.value = UiState.Success(category)
                     }
                 }
-            }
+                .catch { exception ->
+                    // Handle exceptions on the main thread
+                    withContext(Dispatchers.Main) {
+                        _uiStateCategory.value = UiState.Error(handleException(exception))
+                    }
+                }
+                .launchIn(this) // Collect flow in the current coroutine
         }
     }
 
-    }
+
+}
 
 
 
